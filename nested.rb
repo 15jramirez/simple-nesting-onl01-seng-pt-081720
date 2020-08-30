7ccf2dc97cdd4ef5d0d15096f93532890ef6cbcd
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-08-30 23:04:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -101,9 +101,6 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for]= alans_new_info
-     
-     
 end
 
 def adding_to_dennis
@@ -122,5 +119,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    
 end
